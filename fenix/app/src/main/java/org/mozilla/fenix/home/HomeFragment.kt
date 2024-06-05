843a3779b2dbeca6f2da42f1729e9ed7a564874c
Coldsparkle <Coldsparkle@163.com>
2024-06-05 10:59:12 +0800

    feat: change wordmark

--- fenix/app/src/main/java/org/mozilla/fenix/home/HomeFragment.kt
+++ fenix/app/src/main/java/org/mozilla/fenix/home/HomeFragment.kt
@@ -1179,7 +1179,7 @@ class HomeFragment : Fragment() {
             else -> ColorStateList.valueOf(color)
         }
 
-        binding.wordmarkText.imageTintList = tintColor
+//        binding.wordmarkText.imageTintList = tintColor
         binding.privateBrowsingButton.imageTintList = tintColor
     }
 
